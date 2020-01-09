20e341d897d998e0db49f91948a907b5236ff6bf
Mark Witt <markwitt6@yahoo.de>
2020-01-09 08:04:14 -0600

    erste Initialisierung in GameKlasse und Erstellung verschiedener Klassen

---
+++ src/polymony/Field.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polymony;
+
+/**
+ *
+ * @author root
+ */
+public abstract class Field {
+    String name;
+    public Field(String name){
+        this.name = name;
+    }
+}
--- src/polymony/Game.java
+++ src/polymony/Game.java
@@ -9,6 +9,21 @@ package polymony;
  *
  * @author root
  */
-public class Game {
+public class Game implements GameInterface{
+    Player[] players;
+    int currentPlayer;
+    public Game(int playerCount){
+        // erstelle Spieler Array mit angegebener Spieleranzahl
+        this.players = new Player[playerCount];
+        //Fülle den Array mit Spielern
+        for(int i=0;i<playerCount;i++){
+            this.players[i] = new HumanPlayer();
+        }
+        currentPlayer = 0;
+    }
     
+    //Testmethode, um Spiellogik zu testen, ohne Verbindung zur Grafik
+    public static void main(String[] args) {
+        Game g1 = new Game(3);
+    }
 }
