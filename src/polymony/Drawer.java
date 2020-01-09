4114778a7bbac75f5664b08b22f09e344b9c6c98
Joel <joel.rose@gmx.net>
2020-01-09 10:28:48 +0100

    Initial Commit

---
+++ src/polymony/Drawer.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polymony;
+
+/**
+ *
+ * @author joel.rose
+ */
+public interface Drawer {
+    void handle();
+}
---
+++ src/polymony/Main.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polymony;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author joel.rose
+ */
+public class Main extends Application {
+
+    PolyMonyDrawer polyMonyDrawer;
+   
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Group root = new Group();
+        this.polyMonyDrawer = new PolyMonyDrawer(root);
+        
+        primaryStage.setTitle("PolyMony");
+        Button btn = new Button();
+        btn.setText("Say 'OnNewGame'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+               polyMonyDrawer.onNewGame.handle();        
+            }
+        });
+
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 200, 300));
+        primaryStage.show();
+    }
+}
