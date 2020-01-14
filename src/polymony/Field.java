7a01f06a731683b5d190c6f459e28832f4a85413
Tippvehler <Marcel.Juritz@web.de>
2020-01-14 14:03:17 +0100

    Würfelklasse

---
+++ src/polymony/Field.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polymony;
+
+/**
+ *
+ * @author manfr
+ */
+public abstract class Field {
+  int index;
+  String name;
+}
