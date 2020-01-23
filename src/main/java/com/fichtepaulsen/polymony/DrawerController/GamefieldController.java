acf3a34d6fca74053b99536545f2ec48b64567d0
Leander <leanderwellsow@gmail.com>
2020-01-23 10:55:02 +0100

    Comments added to GamefieldController.java

--- src/main/java/com/fichtepaulsen/polymony/DrawerController/GamefieldController.java
+++ src/main/java/com/fichtepaulsen/polymony/DrawerController/GamefieldController.java
@@ -17,10 +17,9 @@ public class GamefieldController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        int x = 0;
-        int y = 0;
-       
-      
+    
+        //Every rectangle is added to a GridPane and is equal to one field
+        //creates corner square (c1) (top left)
         Rectangle c1 = new Rectangle(50.0, 50.0);
         c1.setStrokeWidth(1.0);
         c1.setStrokeType(StrokeType.OUTSIDE);
@@ -28,6 +27,7 @@ public class GamefieldController implements Initializable {
         c1.setFill(Color.WHITE);
         gp.add(c1, 0, 0);
         
+        //creates corner square (c2) (top right)
         Rectangle c2 = new Rectangle(50.0, 50.0);
         c2.setStrokeWidth(1.0);
         c2.setStrokeType(StrokeType.OUTSIDE);
@@ -35,6 +35,7 @@ public class GamefieldController implements Initializable {
         c2.setFill(Color.WHITE);
         gp.add(c2, 10, 0);
         
+        //creates corner square (c3) (bottom left)
         Rectangle c3 = new Rectangle(50.0, 50.0);
         c3.setStrokeWidth(1.0);
         c3.setStrokeType(StrokeType.OUTSIDE);
@@ -42,13 +43,15 @@ public class GamefieldController implements Initializable {
         c3.setFill(Color.WHITE);
         gp.add(c3, 0, 10);
         
+        //creates corner square (c4) (bottom right)
         Rectangle c4 = new Rectangle(50.0, 50.0);
         c4.setStrokeWidth(1.0);
         c4.setStrokeType(StrokeType.OUTSIDE);
         c4.setStroke(Color.BLACK);
         c4.setFill(Color.WHITE);
         gp.add(c4, 10, 10);
-            
+        
+        //creates upper horizontal fields
         for(int i = 1; i<=9; i++) {
                 Rectangle rec = new Rectangle();
                 rec.setHeight(25.0);
@@ -60,38 +63,41 @@ public class GamefieldController implements Initializable {
           
                 gp.add(rec, 0, i);
         }
-            
-            for(int i = 1; i<=9; i++){
-                Rectangle rec = new Rectangle();
-                rec.setHeight(50.0);
-                rec.setWidth(25.0);
-                rec.setStrokeWidth(1.0);
-                rec.setStrokeType(StrokeType.OUTSIDE);
-                rec.setStroke(Color.BLACK);
-                rec.setFill(Color.WHITE);
-                gp.add(rec, i, 0);
-            }
-            
-            for(int i = 1; i<=9; i++) {
-                Rectangle rec = new Rectangle();
-                rec.setHeight(25.0);
-                rec.setWidth(50.0);
-                rec.setStrokeWidth(1.0);
-                rec.setStrokeType(StrokeType.OUTSIDE);
-                rec.setStroke(Color.BLACK);
-                rec.setFill(Color.WHITE);
-                gp.add(rec, 10, i);
-            }
-            
-            for(int i = 1; i<=9; i++){
-                Rectangle rec = new Rectangle();
-                rec.setHeight(50.0);
-                rec.setWidth(25.0);
-                rec.setStrokeWidth(1.0);
-                rec.setStrokeType(StrokeType.OUTSIDE);
-                rec.setStroke(Color.BLACK);
-                rec.setFill(Color.WHITE);
-                gp.add(rec, i, 10);
-            }   
+        
+        //creates left vertical fields
+        for(int i = 1; i<=9; i++){
+            Rectangle rec = new Rectangle();
+            rec.setHeight(50.0);
+            rec.setWidth(25.0);
+            rec.setStrokeWidth(1.0);
+            rec.setStrokeType(StrokeType.OUTSIDE);
+            rec.setStroke(Color.BLACK);
+            rec.setFill(Color.WHITE);
+            gp.add(rec, i, 0);
+        }
+        
+        //creates lower horizontal fields
+        for(int i = 1; i<=9; i++) {
+            Rectangle rec = new Rectangle();
+            rec.setHeight(25.0);
+            rec.setWidth(50.0);
+            rec.setStrokeWidth(1.0);
+            rec.setStrokeType(StrokeType.OUTSIDE);
+            rec.setStroke(Color.BLACK);
+            rec.setFill(Color.WHITE);
+            gp.add(rec, 10, i);
+        }
+        
+        //creates right vertical fields
+        for(int i = 1; i<=9; i++){
+            Rectangle rec = new Rectangle();
+            rec.setHeight(50.0);
+            rec.setWidth(25.0);
+            rec.setStrokeWidth(1.0);
+            rec.setStrokeType(StrokeType.OUTSIDE);
+            rec.setStroke(Color.BLACK);
+            rec.setFill(Color.WHITE);
+            gp.add(rec, i, 10);
+        }   
     }   
 }
