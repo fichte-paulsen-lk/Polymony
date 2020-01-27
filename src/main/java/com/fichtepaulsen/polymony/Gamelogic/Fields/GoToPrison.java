59ae6f3cde01b9181c06e3ce85380fb828580495
Tippvehler <Marcel.Juritz@web.de>
2020-01-27 11:44:51 +0100

    added branch actionFields

--- src/main/java/com/fichtepaulsen/polymony/Gamelogic/Fields/GoToPrison.java
+++ src/main/java/com/fichtepaulsen/polymony/Gamelogic/Fields/GoToPrison.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.fichtepaulsen.polymony.Gamelogic.Fields;
 
-/**
- *
- * @author manfr
- */
-public class GoToPrison {
-    
+import com.fichtepaulsen.polymony.Gamelogic.Player.Player;
+
+public class GoToPrison extends SpecialField {
+    public GoToPrison(){
+}
+    @Override
+    public void action(Player activePlayer){
+        activePlayer.setPosition(10);
+        activePlayer.setIncarcerated(true);
+    }
 }
