7a4b53bbaa1b8492900868ce9ab590c256fb930a
Marvin Fenner <fennro@outlook.com>
2020-03-05 10:36:47 +0000

    added actual cards + bug fixes

--- src/main/java/com/fichtepaulsen/polymony/Gamelogic/Cards/GoToPrisonCard.java
+++ src/main/java/com/fichtepaulsen/polymony/Gamelogic/Cards/GoToPrisonCard.java
@@ -3,15 +3,14 @@ package com.fichtepaulsen.polymony.Gamelogic.Cards;
 import com.fichtepaulsen.polymony.Gamelogic.Game;
 import com.fichtepaulsen.polymony.Gamelogic.Player.Player;
 
-public class PrisonFreeCard extends Card{
+public class GoToPrisonCard  extends Card{
     
-    public PrisonFreeCard (String text,boolean community){
+    public GoToPrisonCard(String text,boolean community){
         super(text,community);
     }
 
     @Override
     public void action(Game game) {
-        Player activePlayer = game.getActivePlayer();
-        activePlayer.setAmountPrisonFreeCard(activePlayer.getAmountPrisonFreeCard()+1);
+        game.getActivePlayer().setInPrison();
     }
 }
--- src/main/java/com/fichtepaulsen/polymony/Gamelogic/Cards/JumpToCard.java
+++ src/main/java/com/fichtepaulsen/polymony/Gamelogic/Cards/JumpToCard.java
@@ -8,17 +8,19 @@ public class JumpToCard extends Card {
 
     private int position; //position on which the player jumps
     private Player[] players;
+    private boolean getMoney; 
 
-    public JumpToCard(int position, String title, boolean community) {
+    public JumpToCard(int position, String title,boolean getMoney, boolean community) {
         super(title, community);
         this.position = position;
+        this.getMoney = getMoney;
     }
 
     @Override
     public void action(Game game) {
         players = game.getAllPlayers();
         Player currentPlayer = game.getCurrentPlayer();
-        /*if(game.passedStart(currentPlayer.getPosition(),position)){
+        /*if(game.passedStart(currentPlayer.getPosition(),position) && getMoney){
         currentPlayer.changeBalance(200);
       }*/
         currentPlayer.setPosition(position);
